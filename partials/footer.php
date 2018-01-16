7870509adf97f057c40063834d5debef84445ee3
Anthony Robledo <tony345.misa@gmail.com>
2018-01-15 20:33:48 -0400

    Se agregaron modals para el inicio de sesion y registro de nuevo usuario

--- partials/footer.php
+++ partials/footer.php
@@ -1,9 +1,9 @@
 <footer class="container text-center">
   <div class="container">
     <div class="row">
-          <div class="col-sm-10">
+        <div class="col-sm-10">
         </div>
-          <div class="col-sm-2">
+        <div class="col-sm-2">
         </div>
       </div>
   </div>
--- partials/navbar.php
+++ partials/navbar.php
@@ -1,3 +1,4 @@
+<?php  session_start(); ?>
 <div class="jumbotron">
   <div class="container text-center">
     <h1>Requisitor De Compras</h1>      
@@ -22,9 +23,51 @@
         <li><a href="#">Contacto</a></li>
       </ul>
       <ul class="nav navbar-nav navbar-right">
-        <li><a href="#"><span class="glyphicon glyphicon-user"></span> Cuenta</a></li>
-        <li><a href="#"><span class="glyphicon glyphicon-shopping-cart"></span> Carrito</a></li>
+        <?php if (isset($_COOKIE['notice'])){ ?>
+          <li><a href="#"><span class="glyphicon glyphicon-user"></span> <?php echo $_COOKIE["nombre"]; ?></a></li>
+          <li><a href="#"><span class="glyphicon glyphicon-shopping-cart"></span> Carrito</a></li>
+        <?php }else{ ?>
+          <li><a href="#" data-toggle="modal" data-target="#loginModal"><span class="glyphicon glyphicon-user"></span> Iniciar Sesión</a></li>
+          <li><a href="#" data-toggle="modal" data-target="#registerModal"><span class="glyphicon glyphicon-user"></span> Registrarse</a></li>
+        <?php } ?>
       </ul>
     </div>
   </div>
 </nav>
+
+
+<div class="modal fade" id="loginModal" role="dialog">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+          <h4 class="modal-title">Iniciar Sesión</h4>
+        </div>
+        <div class="modal-body">
+          <form></form>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-danger" data-dismiss="modal">Cancelar</button>
+          <button type="button" class="btn btn-success" data-dismiss="modal">Enviar</button>
+        </div>
+      </div>
+    </div>
+</div>
+
+<div class="modal fade" id="registerModal" role="dialog">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+          <h4 class="modal-title">Registro de Usuario</h4>
+        </div>
+        <div class="modal-body">
+          <form></form>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-danger" data-dismiss="modal">Cancelar</button>
+          <button type="button" class="btn btn-success" data-dismiss="modal">Enviar</button>
+        </div>
+      </div>
+    </div>
+</div>
