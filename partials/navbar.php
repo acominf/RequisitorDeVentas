2218c1bc4d4320c9439cc440801712012d8b904b
Anthony Robledo <tony345.misa@gmail.com>
2018-01-16 20:59:33 -0400

    Se agregaron archivos php de login y registro

--- partials/navbar.php
+++ partials/navbar.php
@@ -35,39 +35,5 @@
   </div>
 </nav>
 
+<?php include 'partials/modales.php'; ?>
 
-<div class="modal fade" id="loginModal" role="dialog">
-    <div class="modal-dialog">
-      <div class="modal-content">
-        <div class="modal-header">
-          <button type="button" class="close" data-dismiss="modal">&times;</button>
-          <h4 class="modal-title">Iniciar Sesión</h4>
-        </div>
-        <div class="modal-body">
-          <form></form>
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-danger" data-dismiss="modal">Cancelar</button>
-          <button type="button" class="btn btn-success" data-dismiss="modal">Enviar</button>
-        </div>
-      </div>
-    </div>
-</div>
-
-<div class="modal fade" id="registerModal" role="dialog">
-    <div class="modal-dialog">
-      <div class="modal-content">
-        <div class="modal-header">
-          <button type="button" class="close" data-dismiss="modal">&times;</button>
-          <h4 class="modal-title">Registro de Usuario</h4>
-        </div>
-        <div class="modal-body">
-          <form></form>
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-danger" data-dismiss="modal">Cancelar</button>
-          <button type="button" class="btn btn-success" data-dismiss="modal">Enviar</button>
-        </div>
-      </div>
-    </div>
-</div>
